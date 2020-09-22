ff5a33b23d3f4dae8f0948f8042ad6f23e91685a
sham <shamelius77@gmail.com>
2020-09-22 10:24:14 -0500

    Diseño del prototipo

---
+++ ksk-dashboard/src/app/pages/loadplan/loadplan.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loadplan',
+  templateUrl: './loadplan.component.html',
+  styles: [
+  ]
+})
+export class LoadplanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
